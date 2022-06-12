7242af09b81349feddd50b505c4e30e5bc6e5fb3
Matthew Kavanagh <matthew.kavanagh@skybettingandgaming.com>
2022-06-12 12:46:43 +0100

    Blog Schema

--- schemas/projects.ts
+++ schemas/projects.ts
@@ -1,45 +1,98 @@
 import { Rule } from "@sanity/types";
 
+const metaData = [
+  {
+    name: "Title",
+    title: "Title",
+    type: "string",
+    validation: (Rule: Rule) => {
+      return Rule.required().min(5).max(25);
+    },
+  },
+  {
+    name: "coverImage",
+    title: "Cover Image",
+    type: "image",
+    fields: [
+      {
+        type: "text",
+        name: "alt",
+        title: "Description",
+      },
+    ],
+    options: {
+      hotspot: true,
+    },
+  },
+  {
+    name: "subTitle",
+    title: "Subtitle",
+    type: "string",
+  },
+  {
+    name: "slug",
+    title: "Slug",
+    type: "slug",
+    validation: (Rule: Rule) => {
+      return Rule.required().error("You done goofed");
+    },
+  },
+];
+
+const content = [
+  {
+    name: "content",
+    title: "Content",
+    type: "array",
+    of: [
+      {
+        type: "block",
+      },
+      {
+        type: "image",
+        fields: [
+          {
+            name: "caption",
+            title: "Caption",
+            type: "string",
+            options: {
+              isHighlighted: true, // <-- make this field easily accessible
+            },
+          },
+          {
+            title: "Image Position",
+            type: "string",
+            name: "position",
+            options: {
+              list: [
+                { title: "Centre", value: "center" },
+                { title: "Left", value: "left" },
+                { title: "Right", value: "right" },
+              ],
+              layout: "radio",
+              isHighlited: true,
+            },
+          },
+          {
+            type: "text",
+            name: "alt",
+            title: "Description",
+            options: {
+              isHighlighted: true,
+            },
+          },
+        ],
+        options: {
+          hotspot: true,
+        },
+      },
+    ],
+  },
+];
+
 export const projects = {
   name: "projects",
   type: "document",
   title: "Projects",
-  fields: [
-    {
-      name: "Title",
-      title: "Title",
-      type: "string",
-      validation: (Rule: Rule) => {
-        return Rule.required().min(5).max(25);
-      },
-    },
-    {
-      name: "subTitle",
-      title: "Subtitle",
-      type: "string",
-    },
-    {
-      name: "coverImage",
-      title: "Cover Image",
-      type: "image",
-      fields: [
-        {
-          type: "text",
-          name: "alt",
-          title: "Description",
-        },
-      ],
-      options: {
-        hotspot: true,
-      },
-    },
-    {
-      name: "slug",
-      title: "Slug",
-      type: "slug",
-      validation: (Rule: Rule) => {
-        return Rule.required().error("You done goofed");
-      },
-    },
-  ],
+  fields: [...metaData, ...content],
 };
